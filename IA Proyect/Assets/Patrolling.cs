c30e965676cce23dab2ed8356b3fa545760e060f
Ludef26 <Ludef26@gmail.com>
2024-10-10 13:13:51 +0200

    PatrollingAgent & jumps

--- IA Proyect/Assets/Patrolling.cs
+++ IA Proyect/Assets/Patrolling.cs
@@ -1,8 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.AI;
 public class Patrolling : MonoBehaviour
 {
-    
+    public NavMeshAgent patrollingAgent;
+    public Transform ghostAgent;
+
+    private void Update()
+    {
+        patrollingAgent.SetDestination(ghostAgent.position);
+    }
 }
